e4ca17b2ba8fce0980248fb695321c03ceb81d09
robiul1hossen <robim6225@gmail.com>
2023-05-20 02:42:12 +0600

    some data find fucntionality added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
+const toys = "./toys.json";
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dowmgti.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,6 +25,52 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db("miniMotors").collection("allToys");
+
+    app.get("/allToys", async (req, res) => {
+      const cursor = toysCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/allToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        projection: {
+          category: 1,
+          name: 1,
+          price: 1,
+          img: 1,
+          available_quantity: 1,
+          subcategory: 1,
+          rating: 1,
+          sellerName: 1,
+          sellerEmail: 1,
+        },
+      };
+
+      const result = await toysCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    app.get("/allToy", async (req, res) => {
+      let query = {};
+      if (req.query?.sellerEmail) {
+        query = { sellerEmail: req.query.sellerEmail };
+      }
+      const result = await toysCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/allToys", async (req, res) => {
+      const allToys = req.body;
+      console.log(allToys);
+      const result = await toysCollection.insertOne(allToys);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
